4182a8dd0d259ed18069e907e7b762ec56ad6f9c
Terry RPi <mykfc@qq.com>
2015-09-08 07:04:08 +0000

    add lib

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include "/usr/include/mysql/mysql.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
-    return 0;
+	cout << "== AI Begin ==" << endl;
+	const char *host = "robot.mokfc.com";
+	const char *user = "root";
+	const char *pwd = "513939";
+	const char *db = "mysql";
+	MYSQL mysql;
+	mysql_init(&mysql);
+	if (!mysql_real_connect(&mysql, host, user, pwd, db, 0, NULL, 0)) {
+		printf("%s\n", mysql_error(&mysql));
+	} else {
+		printf("Connect successed\n");
+	}
+	mysql_close(&mysql);
+	cout << "=== AI End ===" << endl;
+	return 0;
 }
