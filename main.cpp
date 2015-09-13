87beae5418a679e19b616ee925bf4a2bfcafc907
terry <terry@rokid.com>
2015-09-14 00:58:02 +0800

    just fix a simple bug

--- main.cpp
+++ main.cpp
@@ -67,7 +67,7 @@ bool is_normal_zh_symbol(char a_char_0, char a_char_1, char a_char_2)
 	return false;
 }
 
-word *check_word(utfstring *utfword, )
+word *check_word(utfstring *utfword)
 {
 	mysql_select_db(&mysql, "corpus");
 	sprintf(query_buf,"select * from word_sogou where word_sogou.word = '%s' limit 1;",utfword->c_str());
